d28c3ae5e67a9d5dc029bc5a094e5b9164881d3f
thakurankit012 <157781047+thakurankit012@users.noreply.github.com>
2024-03-29 12:05:05 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,18 +1,17 @@
 Array.prototype.Myfilter = function(callback) {
-  var novoArray = []
-  
-  for (let indice = 0; indice < this.length; indice++){
-    const filtro = callback(this[indice], indice, this)
+  var novoArray = []; // Create an empty array to store filtered elements
 
-    if (filtro){
-      novoArray.push(this[indice])
-    } 
+  for (let indice = 0; indice < this.length; indice++) {
+    const filtro = callback(this[indice], indice, this); // Call the callback function with current element, index, and the array itself
+
+    if (filtro) {
+      novoArray.push(this[indice]); // If the callback returns true, add the element to the new array
+    }
   }
 
-  return novoArray;
+  return novoArray; // Return the new filtered array
 } 
 
+const teste = [10, 1, 2, 3, 4, 5, 8];
 
-const teste = [10, 1, 2, 3, 4, 5, 8]
-
-console.log(teste.Myfilter((n, index, array) => n > 4))
+console.log(teste.Myfilter((n, index, array) => n > 4)); // Filter elements greater than 4
