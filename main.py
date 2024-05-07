1ea07f1b0fc9ee0b6d0a32b38f03ba234b17f26d
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-07 21:57:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ print(hello world)
 # print numbers 
 print(33)
 print(24)
-# basic mathematical calculation 
+# Basic mathematical calculation 
 print(33+24)
 print(33-24)
 print(33*24)
 print(33/24)
+# variables in Python - I already know Java and C++ so I am a little bit laughing about it but ya it is also a programming language so I respect that Python is easy for beginners
+# So how would I choose var i just have to give a (random)name then = value that I want to store basically in the form of data types
+car = "Mercedes"  #  a variable stored in the memory of computers if you know fundamentals well;
+color = "brownish"
+price = 2 # Let us say 2 stands for 2 cr in numeric datatype
+
+print(car)
+print(price)
+print("car") # This will assume that the user wants to print a string because it is in double quotation marks so the output will be car instead of value Mercedes
 
