a6822f48a0eb47d297730354650bbb30417a1393
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-07 21:31:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-# thr first python language practice 
-#start with hello world created on 2-02-2024
+# the first Python language practice 
+#start with Hello World created on 2-02-2024
 print(hello world)
 # print numbers 
 print(33)
 print(24)
+# basic mathematical calculation 
+print(33+24)
+print(33-24)
+print(33*24)
+print(33/24)
+
