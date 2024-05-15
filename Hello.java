fb17ef406d456c2a6dcebe1231c85593db59dc3e
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-15 21:55:11 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,28 @@
+class hello {
+    public static void main (String b[])
+    {
+        int ankit = 9;
+        double myInt = ankit;
+
+        System.out.println(ankit);
+        System.out.println(myInt);
+    }
+}
+// java is basicaaly run in oops concept oops designed as object oriente programming system
+class Hello
+
+{
+   public static void main (String a[])
+   {
+    //literals
+     char c = 'c';
+     c++;
+     System.out.println(c);
+
+
+
+
+
+   }
+     
+}
