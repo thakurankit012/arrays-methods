a69e41a087fac60364be094b855fca90fc56f24b
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-15 21:59:40 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -20,9 +20,50 @@ class Hello
      System.out.println(c);
 
 
-
-
-
    }
      
+}
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2, result;
+        char operator;
+
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        num2 = scanner.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error! Division by zero is not allowed.");
+                } else {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                }
+                break;
+            default:
+                System.out.println("Error! Invalid operator.");
+        }
+    }
 }
