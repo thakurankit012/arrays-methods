00b805a29a780097b6f251c316f7da6ce362ae83
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-29 09:36:54 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -18,3 +18,24 @@ public class Patterns {
         }
     }
 }
+
+import java.util.*;
+
+public class patterns {
+
+     public static void main (String[] args) {
+
+         int n = 4;
+
+         //outer loop
+         for (int i = 1; i <= n; i++) {
+             for (int j = 1; j <= i; j++) {
+
+                     System.out.print("*");
+                 }
+             System.out.println();
+         }
+     }
+}
+
+
