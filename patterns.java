ec31d6b69df87a3611774cdaac4231645bfb28e4
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-29 09:39:07 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -1,11 +1,11 @@
 public class Patterns {
 
     public static void main(String[] args) {
-        int n = 4; // Number of rows
+        int n = 4; 
 
-        // Outer loop for rows
+        // Outer loop 
         for (int i = 1; i <= n; i++) {
-            // Inner loop for spaces
+            // Inner loop f
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
@@ -13,7 +13,7 @@ public class Patterns {
             for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
-            // Move to the next line after each row is printed
+        
             System.out.println();
         }
     }
